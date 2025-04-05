e70c42fdcd84a2a278fbd8e6927770446a9b0a2e
SamaraHacker <samara28@iastate.edu>
2025-04-04 20:06:29 -0500

    Inital commit

---
+++ CyHunt/app/src/main/java/com/example/cyhunt/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.cyhunt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        // Set Pantry selected
+        bottomNavigationView.setSelectedItemId(R.id.map_home);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.hunts_home) {
+                    startActivity(new Intent(getApplicationContext(), HuntsActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                } else if (itemId == R.id.map_home) {
+                    return true;
+                } else if (itemId == R.id.profile_home) {
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+}
